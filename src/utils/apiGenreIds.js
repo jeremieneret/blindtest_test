e7022e933592ee6279d7db3e3db3d7985e874617
Jérémie Néret <60704529+jeremieneret@users.noreply.github.com>
2021-12-21 17:57:44 +0100

    Update apiGenreIds.js

--- src/utils/apiGenreIds.js
+++ src/utils/apiGenreIds.js
@@ -1,4 +1,4 @@
 // the integers of the array are the ids of a selection of genres in Deezer Api
 const apiGenreIds = [85, 152, 466, 129, 98, 169, 2]
-// we will use them as id for our fetches, randomly, thanks top the function below
+// we will use them as id for our fetches, randomly, thanks to the function below
 export const shuffledApiGenreIds = apiGenreIds.sort(() => Math.random() - 0.5)
